9e13bfa2a2d16a8955c5cd40b782c0b6b4302e14
Clark Kampfe <clark.kampfe@gmail.com>
2025-05-21 00:07:57 -0500

    report spec failures

--- lib/spec.ex
+++ lib/spec.ex
@@ -22,118 +22,255 @@ defmodule Spec do
     end
   end
 
-  def conform(m, f, _value) when is_atom(m) and is_atom(f) do
+  def conform(m, f, value) when is_atom(m) and is_atom(f) do
+    do_conform(&m.f/1, value, [])
   end
 
   def conform(spec, value) when is_function(spec, 1) do
-    if spec.(value) do
-      value
-    else
-      Spec.Invalid
-    end
+    do_conform(spec, value, [])
   end
 
-  def conform({:all, specs}, value) do
-    Enum.reduce_while(specs, value, fn spec, acc ->
-      conformed = conform(spec, acc)
-
-      if conformed == Spec.Invalid do
-        {:halt, conformed}
-      else
-        {:cont, conformed}
-      end
-    end)
+  def conform({:all, _specs} = expr, value) do
+    do_conform(expr, value, [])
   end
 
-  def conform({:any, specs_and_keys}, value) do
-    Enum.find_value(specs_and_keys, Spec.Invalid, fn {key, spec} ->
-      conformed = conform(spec, value)
-
-      if conformed != Spec.Invalid do
-        {key, conformed}
-      else
-        false
-      end
-    end)
+  def conform({:any, _specs_and_keys} = expr, value) do
+    do_conform(expr, value, [])
   end
 
   def conform(%MapSet{} = spec, value) do
-    if MapSet.member?(spec, value) do
-      value
-    else
-      Spec.Invalid
-    end
+    do_conform(spec, value, [])
   end
 
-  def conform({:keys, specs}, value) when is_map(value) do
-    req_specs = specs[:required]
-    opt_specs = specs[:optional]
-    req_keyset = MapSet.new(Keyword.keys(req_specs))
-    keys = MapSet.new(Map.keys(value))
-
-    if !Enum.empty?(req_keyset) &&
-         MapSet.equal?(
-           MapSet.intersection(req_keyset, keys),
-           req_keyset
-         ) do
-      req_conform =
-        Enum.reduce_while(req_specs, value, fn {key, spec}, acc ->
-          value_for_key = Map.fetch!(acc, key)
-          conformed = conform(spec, value_for_key)
-
-          if conformed == Spec.Invalid do
-            {:halt, conformed}
-          else
-            {:cont, Map.put(acc, key, conformed)}
-          end
-        end)
-
-      if req_conform == Spec.Invalid do
-        Spec.Invalid
-      else
-        Enum.reduce_while(opt_specs, req_conform, fn {key, spec}, acc ->
-          case Map.fetch(acc, key) do
-            :error ->
-              {:cont, acc}
-
-            {:ok, value_for_key} ->
-              conformed = conform(spec, value_for_key)
-
-              if conformed == Spec.Invalid do
-                {:halt, conformed}
-              else
-                {:cont, Map.put(acc, key, conformed)}
-              end
-          end
-        end)
-      end
-    else
-      Spec.Invalid
-    end
+  def conform({:keys, _specs} = expr, value) when is_map(value) do
+    do_conform(expr, value, [])
   end
 
   def conform({:keys, _specs}, value) when not is_map(value) do
     Spec.Invalid
   end
 
+  # defp do_conform(m, f, value, path) when is_atom(m) and is_atom(f) do
+  #   if m.f(value) do
+  #     value
+  #   else
+  #     Spec.Invalid
+  #   end
+  # end
+
+  defp do_conform(spec, value, path) when is_function(spec, 1) do
+    if spec.(value) do
+      {:ok, value}
+    else
+      Spec.Invalid
+      {:error, %{spec: spec, value: value, path: path}}
+    end
+  end
+
+  defp do_conform({:keys, specs} = spec, value, path) when is_map(value) do
+    req_specs = specs[:required]
+    opt_specs = specs[:optional]
+    keys = MapSet.new(Map.keys(value))
+
+    req_keyset = specs[:required] |> Keyword.keys() |> MapSet.new()
+
+    opt_keyset =
+      specs[:optional] |> Keyword.keys() |> MapSet.new()
+
+    common_keys =
+      MapSet.intersection(req_keyset, opt_keyset)
+
+    if Enum.empty?(req_keyset) && Enum.empty?(opt_keyset) do
+      throw(
+        {:error,
+         %{
+           spec: spec,
+           value: value,
+           path: path,
+           error: "required and optional keysets cannot both be empty"
+         }}
+      )
+    end
+
+    if !MapSet.equal?(common_keys, MapSet.new()) do
+      throw(
+        {:error,
+         %{
+           spec: spec,
+           value: value,
+           path: path,
+           error: "required keys cannot alows be optional keys"
+         }}
+      )
+    end
+
+    # TODO
+    # if required keyset is not a subset of value keyset, raise
+    if !MapSet.subset?(req_keyset, keys) do
+      diff = MapSet.difference(req_keyset, keys)
+
+      throw(
+        {:error,
+         %{spec: spec, value: value, path: path, error: "missing #{inspect(Enum.to_list(diff))}"}}
+      )
+    end
+
+    # TODO
+    # req keys and opt keys cannot overlap
+
+    # TODO
+    # req keys can be empty
+
+    if MapSet.equal?(
+         MapSet.intersection(req_keyset, keys),
+         req_keyset
+       ) do
+      {req_conform, errors} =
+        Enum.reduce_while(req_specs, {value, []}, fn {key, spec}, {acc, errors} ->
+          value_for_key = Map.fetch!(acc, key)
+
+          case do_conform(spec, value_for_key, path) do
+            {:ok, conformed} ->
+              {:cont, {Map.put(acc, key, conformed), errors}}
+
+            {:error, e} ->
+              e =
+                Map.update!(e, :path, fn path ->
+                  [key | path]
+                end)
+
+              # {:halt, {:error, e}}
+              # {:cont, }
+              {:cont, {acc, [e | errors]}}
+          end
+        end)
+
+      {out, errors} =
+        Enum.reduce_while(opt_specs, {req_conform, errors}, fn {key, spec}, {acc, errors} ->
+          case Map.fetch(acc, key) do
+            :error ->
+              {:cont, {acc, errors}}
+
+            {:ok, value_for_key} ->
+              case do_conform(spec, value_for_key, path) do
+                {:ok, conformed} ->
+                  {:cont, {Map.put(acc, key, conformed), errors}}
+
+                {:error, e} ->
+                  e =
+                    Map.update!(e, :path, fn path ->
+                      [key | path]
+                    end)
+
+                  {:cont, {acc, [e | errors]}}
+              end
+          end
+        end)
+
+      if !Enum.empty?(errors) do
+        {:error, errors}
+      else
+        {:ok, out}
+      end
+    else
+      Spec.Invalid
+    end
+  catch
+    :throw, e ->
+      e
+  end
+
+  defp do_conform(%MapSet{} = spec, value, path) do
+    if MapSet.member?(spec, value) do
+      {:ok, value}
+    else
+      {:error, %{value: value, spec: spec, path: path}}
+    end
+  end
+
+  defp do_conform({:all, specs}, value, path) do
+    out =
+      Enum.reduce_while(specs, value, fn spec, acc ->
+        # conformed = do_conform(spec, acc, path)
+
+        case do_conform(spec, acc, path) do
+          {:ok, conformed} ->
+            {:cont, conformed}
+
+          {:error, e} ->
+            {:halt, {:error, e}}
+        end
+      end)
+
+    case out do
+      {:error, _} = e -> e
+      ok -> {:ok, ok}
+    end
+  end
+
+  defp do_conform({:any, specs_and_keys}, value, path) do
+    # Enum.find_value(specs_and_keys, Spec.Invalid, fn {key, spec} ->
+    #   # conformed = do_conform(spec, value, path)
+
+    #   # if conformed != Spec.Invalid do
+    #   #   {key, conformed}
+    #   # else
+    #   #   false
+    #   # end
+
+    #   case do_conform(spec, value, path) do
+    #     {:ok, conformed} ->
+    #       {:ok, {key, conformed}}
+
+    #     {:error, e} ->
+    #       false
+    #   end
+    # end)
+    out =
+      Enum.reduce_while(specs_and_keys, [], fn {key, spec}, errors ->
+        case do_conform(spec, value, path) do
+          {:ok, conformed} ->
+            {:halt, {:ok, {key, conformed}}}
+
+          {:error, e} ->
+            e =
+              Map.update!(e, :path, fn path ->
+                [key | path]
+              end)
+
+            {:cont, [e | errors]}
+        end
+      end)
+
+    case out do
+      {:ok, _} = ok -> ok
+      errors -> {:error, errors}
+    end
+  end
+
   def valid?(%MapSet{} = spec, value) do
-    conform(spec, value) != Spec.Invalid
+    # conform(spec, value) != Spec.Invalid
+    match?({:ok, _}, conform(spec, value))
   end
 
   def valid?({:keys, _specs} = keys_expr, value) do
-    conform(keys_expr, value) != Spec.Invalid
+    # conform(keys_expr, value) != Spec.Invalid
+    match?({:ok, _}, conform(keys_expr, value))
   end
 
   def valid?({:all, _specs} = and_expr, value) do
-    conform(and_expr, value) != Spec.Invalid
+    # conform(and_expr, value) != Spec.Invalid
+    match?({:ok, _}, conform(and_expr, value))
   end
 
   def valid?({:any, _specs} = or_expr, value) do
-    conform(or_expr, value) != Spec.Invalid
+    # conform(or_expr, value) != Spec.Invalid
+    match?({:ok, _}, conform(or_expr, value))
   end
 
   def valid?(spec, value) when is_function(spec, 1) do
-    !!spec.(value)
+    # !!spec.(value)
+    match?({:ok, _}, conform(spec, value))
   end
 
   def valid?(m, f, _value) when is_atom(m) and is_atom(f) do
--- test/spec_test.exs
+++ test/spec_test.exs
@@ -12,17 +12,17 @@ defmodule SpecTest do
   end
 
   test "conform/2" do
-    assert Spec.conform(&Integer.is_even/1, 2) == 2
-    assert Spec.conform(&Integer.is_even/1, 1) == Spec.Invalid
-    assert Spec.conform(MapSet.new([1, 2, 3]), 1) == 1
-    assert Spec.conform(MapSet.new([1, 2, 3]), 9) == Spec.Invalid
+    assert Spec.conform(&Integer.is_even/1, 2) == {:ok, 2}
+    assert match?({:error, %{value: 1}}, Spec.conform(&Integer.is_even/1, 1))
+    assert Spec.conform(MapSet.new([1, 2, 3]), 1) == {:ok, 1}
+    assert match?({:error, _}, Spec.conform(MapSet.new([1, 2, 3]), 9))
   end
 
   test "and" do
     specs = Spec.all?([&Integer.is_even/1, fn v -> v < 10 end])
 
-    assert Spec.conform(specs, 4) == 4
-    assert Spec.conform(specs, 12) == Spec.Invalid
+    assert Spec.conform(specs, 4) == {:ok, 4}
+    assert match?({:error, _}, Spec.conform(specs, 12))
 
     assert Spec.valid?(specs, 4)
     refute Spec.valid?(specs, 12)
@@ -31,16 +31,85 @@ defmodule SpecTest do
   test "or" do
     spec = Spec.any?(even: &Integer.is_even/1, less_than_10: fn v -> v < 10 end)
 
-    assert Spec.conform(spec, 4) == {:even, 4}
-    assert Spec.conform(spec, 9) == {:less_than_10, 9}
-    assert Spec.conform(spec, 12) == {:even, 12}
+    assert Spec.conform(spec, 4) == {:ok, {:even, 4}}
+    assert Spec.conform(spec, 9) == {:ok, {:less_than_10, 9}}
+    assert Spec.conform(spec, 12) == {:ok, {:even, 12}}
 
     assert Spec.valid?(spec, 4)
     assert Spec.valid?(spec, 12)
     refute Spec.valid?(spec, 31)
   end
 
-  test "keys" do
+  test "keys required only" do
+    spec =
+      Spec.keys(
+        required: [
+          a: Spec.all?([&is_binary/1, fn v -> String.length(v) < 5 end]),
+          b: &is_integer/1
+        ],
+        optional: []
+      )
+
+    assert Spec.conform(spec, %{a: "hi", b: 8}) == {:ok, %{a: "hi", b: 8}}
+
+    assert {:error, [%{value: "longer than 5", path: [:a], spec: _}]} =
+             Spec.conform(spec, %{a: "longer than 5", b: 8})
+
+    assert Spec.conform(spec, %{a: "hi", b: 8}) == {:ok, %{a: "hi", b: 8}}
+
+    assert {:error,
+            %{
+              error: "missing [:b]",
+              value: %{a: "hi"},
+              path: [],
+              spec: ^spec
+            }} = Spec.conform(spec, %{a: "hi"})
+
+    assert {:error,
+            [
+              %{value: "not an int", path: [:b], spec: _},
+              %{value: 842_848_024, path: [:a], spec: _}
+            ]} = Spec.conform(spec, %{a: 842_848_024, b: "not an int"})
+
+    assert Spec.conform(spec, %{a: "hi", b: 8, c: "something"}) ==
+             {:ok, %{a: "hi", b: 8, c: "something"}}
+  end
+
+  test "test optional only" do
+    spec =
+      Spec.keys(
+        required: [],
+        optional: [
+          a: Spec.all?([&is_binary/1, fn v -> String.length(v) < 5 end]),
+          b: &is_integer/1,
+          c: &is_atom/1
+        ]
+      )
+
+    assert Spec.conform(spec, %{a: "hi", b: 8}) == {:ok, %{a: "hi", b: 8}}
+
+    assert {:error,
+            [
+              %{
+                value: "longer than 5",
+                path: [:a],
+                spec: _
+              }
+            ]} =
+             Spec.conform(spec, %{a: "longer than 5", b: 8})
+
+    assert Spec.conform(spec, %{a: "hi", b: 8}) == {:ok, %{a: "hi", b: 8}}
+
+    assert Spec.conform(spec, %{a: "hi"}) == {:ok, %{a: "hi"}}
+
+    assert {:error,
+            [
+              %{value: "not an int", path: [:b], spec: _},
+              %{value: 842_848_024, path: [:a], spec: _}
+            ]} = Spec.conform(spec, %{a: 842_848_024, b: "not an int"})
+  end
+
+  test "keys both" do
     spec =
       Spec.keys(
         required: [
@@ -51,14 +120,31 @@ defmodule SpecTest do
       )
 
     # required keys only
-    assert Spec.conform(spec, %{a: "hi", b: 8}) == %{a: "hi", b: 8}
-    assert Spec.conform(spec, %{a: "longer than 5", b: 8}) == Spec.Invalid
-    assert Spec.conform(spec, %{a: "hi"}) == Spec.Invalid
-    assert Spec.conform(spec, %{a: 842_848_024, b: "not an int"}) == Spec.Invalid
+    assert Spec.conform(spec, %{a: "hi", b: 8}) == {:ok, %{a: "hi", b: 8}}
+
+    assert {:error, [%{value: "longer than 5", path: [:a], spec: _}]} =
+             Spec.conform(spec, %{a: "longer than 5", b: 8})
+
+    assert {
+             :error,
+             [
+               %{
+                 path: [:b],
+                 value: "not an int",
+                 spec: _
+               },
+               %{
+                 path: [:a],
+                 value: 842_848_024,
+                 spec: _
+               }
+             ]
+           } =
+             Spec.conform(spec, %{a: 842_848_024, b: "not an int"})
 
     # with opt
-    assert Spec.conform(spec, %{a: "hi", b: 8, c: :foo}) == %{a: "hi", b: 8, c: :foo}
-    assert Spec.conform(spec, %{a: "hi", b: 8, c: "nope"}) == Spec.Invalid
+    assert Spec.conform(spec, %{a: "hi", b: 8, c: :foo}) == {:ok, %{a: "hi", b: 8, c: :foo}}
+    assert match?({:error, _}, Spec.conform(spec, %{a: "hi", b: 8, c: "nope"}))
 
     # valid?
     assert Spec.valid?(spec, %{a: "hi", b: 8})
